542b0d2bc9424dafd5085265d14639eae4561277
Yazawa-ctrl <84038454+Yazawa-ctrl@users.noreply.github.com>
2021-05-12 16:52:40 +0200

    Update fail.cpp

--- fail.cpp
+++ fail.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-int Static[5];
+int Static[101];
 int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen, 
 	int* arrayHeap, unsigned int arrayHeapLen) {
-  int Stack[5];
+  int Stack[101];
 
   Static[100] = 0; 
   Stack[100] = 0; 
 
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
 
-  int* array = new int[5];
+  int* array = new int[101];
   array[100] = 0; 
 
   arrayStack[100] = 0; 
@@ -26,9 +26,9 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
 }
 
 int main(void) {
-  int arrayStack[5];
-  int* arrayHeap = new int[5];
-  memcheckFailDemo(arrayStack, 5, arrayHeap, 5);
+  int arrayStack[101];
+  int* arrayHeap = new int[101];
+  memcheckFailDemo(arrayStack, 100, arrayHeap, 100);
   return 0;
 }
 
